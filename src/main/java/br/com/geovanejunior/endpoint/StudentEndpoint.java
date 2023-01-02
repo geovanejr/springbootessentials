69d93d0b18f5095d2274ee325506ac640f0fabdb
Geovane Junior <geovane.gjunior@gmail.com>
2023-01-02 16:46:48 -0300

    Spring Boot Essentials 09 - Adicionando Spring Data JPA com MySQL pt 02

--- src/main/java/br/com/geovanejunior/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/geovanejunior/endpoint/StudentEndpoint.java
@@ -39,6 +39,12 @@ public class StudentEndpoint {
         return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
+    @GetMapping(path = "/findByName/{name}")
+    public ResponseEntity<?> findStudentByName(@PathVariable("name") String name) {
+
+        return new ResponseEntity<>(studentDAO.findByNameIgnoreCaseContaining(name), HttpStatus.OK);
+    }
+
     @PostMapping
     public ResponseEntity<?> save(@RequestBody Student student) {
 
